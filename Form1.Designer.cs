a1ed9b64fea33013a0542c2806d7e89e2a4cf468
Franklyn <franklynvasquez24@gmail.com>
2021-10-14 16:01:27 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace The_Lottery_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawOne = new System.Windows.Forms.Button();
+            this.drawTwo = new System.Windows.Forms.Button();
+            this.drawThree = new System.Windows.Forms.Button();
+            this.drawFour = new System.Windows.Forms.Button();
+            this.drawFive = new System.Windows.Forms.Button();
+            this.drawSix = new System.Windows.Forms.Button();
+            this.drawSeven = new System.Windows.Forms.Button();
+            this.drawEight = new System.Windows.Forms.Button();
+            this.pickEight = new System.Windows.Forms.Button();
+            this.pickSeven = new System.Windows.Forms.Button();
+            this.pickSix = new System.Windows.Forms.Button();
+            this.pickFive = new System.Windows.Forms.Button();
+            this.pickFour = new System.Windows.Forms.Button();
+            this.pickThree = new System.Windows.Forms.Button();
+            this.pickTwo = new System.Windows.Forms.Button();
+            this.pickOne = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.autoResetButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawOne
+            // 
+            this.drawOne.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawOne.Enabled = false;
+            this.drawOne.Location = new System.Drawing.Point(12, 12);
+            this.drawOne.Name = "drawOne";
+            this.drawOne.Size = new System.Drawing.Size(40, 40);
+            this.drawOne.TabIndex = 0;
+            this.drawOne.Text = "D1";
+            this.drawOne.UseVisualStyleBackColor = false;
+            // 
+            // drawTwo
+            // 
+            this.drawTwo.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawTwo.Enabled = false;
+            this.drawTwo.Location = new System.Drawing.Point(58, 12);
+            this.drawTwo.Name = "drawTwo";
+            this.drawTwo.Size = new System.Drawing.Size(40, 40);
+            this.drawTwo.TabIndex = 1;
+            this.drawTwo.Text = "D2";
+            this.drawTwo.UseVisualStyleBackColor = false;
+            // 
+            // drawThree
+            // 
+            this.drawThree.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawThree.Enabled = false;
+            this.drawThree.Location = new System.Drawing.Point(104, 12);
+            this.drawThree.Name = "drawThree";
+            this.drawThree.Size = new System.Drawing.Size(40, 40);
+            this.drawThree.TabIndex = 2;
+            this.drawThree.Text = "D3";
+            this.drawThree.UseVisualStyleBackColor = false;
+            // 
+            // drawFour
+            // 
+            this.drawFour.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawFour.Enabled = false;
+            this.drawFour.Location = new System.Drawing.Point(150, 12);
+            this.drawFour.Name = "drawFour";
+            this.drawFour.Size = new System.Drawing.Size(40, 40);
+            this.drawFour.TabIndex = 3;
+            this.drawFour.Text = "D4";
+            this.drawFour.UseVisualStyleBackColor = false;
+            // 
+            // drawFive
+            // 
+            this.drawFive.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawFive.Enabled = false;
+            this.drawFive.Location = new System.Drawing.Point(196, 12);
+            this.drawFive.Name = "drawFive";
+            this.drawFive.Size = new System.Drawing.Size(40, 40);
+            this.drawFive.TabIndex = 4;
+            this.drawFive.Text = "D5";
+            this.drawFive.UseVisualStyleBackColor = false;
+            // 
+            // drawSix
+            // 
+            this.drawSix.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawSix.Enabled = false;
+            this.drawSix.Location = new System.Drawing.Point(242, 12);
+            this.drawSix.Name = "drawSix";
+            this.drawSix.Size = new System.Drawing.Size(40, 40);
+            this.drawSix.TabIndex = 5;
+            this.drawSix.Text = "D6";
+            this.drawSix.UseVisualStyleBackColor = false;
+            // 
+            // drawSeven
+            // 
+            this.drawSeven.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawSeven.Enabled = false;
+            this.drawSeven.Location = new System.Drawing.Point(288, 12);
+            this.drawSeven.Name = "drawSeven";
+            this.drawSeven.Size = new System.Drawing.Size(40, 40);
+            this.drawSeven.TabIndex = 6;
+            this.drawSeven.Text = "D7";
+            this.drawSeven.UseVisualStyleBackColor = false;
+            // 
+            // drawEight
+            // 
+            this.drawEight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.drawEight.Enabled = false;
+            this.drawEight.Location = new System.Drawing.Point(334, 12);
+            this.drawEight.Name = "drawEight";
+            this.drawEight.Size = new System.Drawing.Size(40, 40);
+            this.drawEight.TabIndex = 7;
+            this.drawEight.Text = "D8";
+            this.drawEight.UseVisualStyleBackColor = false;
+            // 
+            // pickEight
+            // 
+            this.pickEight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickEight.Location = new System.Drawing.Point(334, 58);
+            this.pickEight.Name = "pickEight";
+            this.pickEight.Size = new System.Drawing.Size(40, 40);
+            this.pickEight.TabIndex = 15;
+            this.pickEight.Text = "P8";
+            this.pickEight.UseVisualStyleBackColor = false;
+            this.pickEight.Click += new System.EventHandler(this.pickEight_Click);
+            // 
+            // pickSeven
+            // 
+            this.pickSeven.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickSeven.Location = new System.Drawing.Point(288, 58);
+            this.pickSeven.Name = "pickSeven";
+            this.pickSeven.Size = new System.Drawing.Size(40, 40);
+            this.pickSeven.TabIndex = 14;
+            this.pickSeven.Text = "P7";
+            this.pickSeven.UseVisualStyleBackColor = false;
+            this.pickSeven.Click += new System.EventHandler(this.pickSeven_Click);
+            // 
+            // pickSix
+            // 
+            this.pickSix.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickSix.Location = new System.Drawing.Point(242, 58);
+            this.pickSix.Name = "pickSix";
+            this.pickSix.Size = new System.Drawing.Size(40, 40);
+            this.pickSix.TabIndex = 13;
+            this.pickSix.Text = "P6";
+            this.pickSix.UseVisualStyleBackColor = false;
+            this.pickSix.Click += new System.EventHandler(this.pickSix_Click);
+            // 
+            // pickFive
+            // 
+            this.pickFive.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickFive.Location = new System.Drawing.Point(196, 58);
+            this.pickFive.Name = "pickFive";
+            this.pickFive.Size = new System.Drawing.Size(40, 40);
+            this.pickFive.TabIndex = 12;
+            this.pickFive.Text = "P5";
+            this.pickFive.UseVisualStyleBackColor = false;
+            this.pickFive.Click += new System.EventHandler(this.pickFive_Click);
+            // 
+            // pickFour
+            // 
+            this.pickFour.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickFour.Location = new System.Drawing.Point(150, 58);
+            this.pickFour.Name = "pickFour";
+            this.pickFour.Size = new System.Drawing.Size(40, 40);
+            this.pickFour.TabIndex = 11;
+            this.pickFour.Text = "P4";
+            this.pickFour.UseVisualStyleBackColor = false;
+            this.pickFour.Click += new System.EventHandler(this.pickFour_Click);
+            // 
+            // pickThree
+            // 
+            this.pickThree.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickThree.Location = new System.Drawing.Point(104, 58);
+            this.pickThree.Name = "pickThree";
+            this.pickThree.Size = new System.Drawing.Size(40, 40);
+            this.pickThree.TabIndex = 10;
+            this.pickThree.Text = "P3";
+            this.pickThree.UseVisualStyleBackColor = false;
+            this.pickThree.Click += new System.EventHandler(this.pickThree_Click);
+            // 
+            // pickTwo
+            // 
+            this.pickTwo.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickTwo.Location = new System.Drawing.Point(58, 58);
+            this.pickTwo.Name = "pickTwo";
+            this.pickTwo.Size = new System.Drawing.Size(40, 40);
+            this.pickTwo.TabIndex = 9;
+            this.pickTwo.Text = "P2";
+            this.pickTwo.UseVisualStyleBackColor = false;
+            this.pickTwo.Click += new System.EventHandler(this.pickTwo_Click);
+            // 
+            // pickOne
+            // 
+            this.pickOne.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pickOne.Location = new System.Drawing.Point(12, 58);
+            this.pickOne.Name = "pickOne";
+            this.pickOne.Size = new System.Drawing.Size(40, 40);
+            this.pickOne.TabIndex = 8;
+            this.pickOne.Text = "P1";
+            this.pickOne.UseVisualStyleBackColor = false;
+            this.pickOne.Click += new System.EventHandler(this.pickOne_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.playButton.Location = new System.Drawing.Point(380, 12);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(80, 40);
+            this.playButton.TabIndex = 16;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // autoResetButton
+            // 
+            this.autoResetButton.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.autoResetButton.Location = new System.Drawing.Point(380, 58);
+            this.autoResetButton.Name = "autoResetButton";
+            this.autoResetButton.Size = new System.Drawing.Size(80, 40);
+            this.autoResetButton.TabIndex = 17;
+            this.autoResetButton.Text = "Auto-Reset";
+            this.autoResetButton.UseVisualStyleBackColor = false;
+            this.autoResetButton.Click += new System.EventHandler(this.autoResetButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(466, 12);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(40, 86);
+            this.resetButton.TabIndex = 18;
+            this.resetButton.Text = "RES";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 120);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.autoResetButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.pickEight);
+            this.Controls.Add(this.pickSeven);
+            this.Controls.Add(this.pickSix);
+            this.Controls.Add(this.pickFive);
+            this.Controls.Add(this.pickFour);
+            this.Controls.Add(this.pickThree);
+            this.Controls.Add(this.pickTwo);
+            this.Controls.Add(this.pickOne);
+            this.Controls.Add(this.drawEight);
+            this.Controls.Add(this.drawSeven);
+            this.Controls.Add(this.drawSix);
+            this.Controls.Add(this.drawFive);
+            this.Controls.Add(this.drawFour);
+            this.Controls.Add(this.drawThree);
+            this.Controls.Add(this.drawTwo);
+            this.Controls.Add(this.drawOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawOne;
+        private System.Windows.Forms.Button drawTwo;
+        private System.Windows.Forms.Button drawThree;
+        private System.Windows.Forms.Button drawFour;
+        private System.Windows.Forms.Button drawFive;
+        private System.Windows.Forms.Button drawSix;
+        private System.Windows.Forms.Button drawSeven;
+        private System.Windows.Forms.Button drawEight;
+        private System.Windows.Forms.Button pickEight;
+        private System.Windows.Forms.Button pickSeven;
+        private System.Windows.Forms.Button pickSix;
+        private System.Windows.Forms.Button pickFive;
+        private System.Windows.Forms.Button pickFour;
+        private System.Windows.Forms.Button pickThree;
+        private System.Windows.Forms.Button pickTwo;
+        private System.Windows.Forms.Button pickOne;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button autoResetButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace The_Lottery_Machine
+{
+    public partial class Form1 : Form
+    {
+        bool autoReset = false;
+        byte picks;
+        byte draw;
+
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            initButtonColors();
+        }
+
+        void initButtonColors()
+        {
+            pickOne.BackColor = Color.SlateGray;
+            pickTwo.BackColor = Color.SlateGray;
+            pickThree.BackColor = Color.SlateGray;
+            pickFour.BackColor = Color.SlateGray;
+            pickFive.BackColor = Color.SlateGray;
+            pickSix.BackColor = Color.SlateGray;
+            pickSeven.BackColor = Color.SlateGray;
+            pickEight.BackColor = Color.SlateGray;
+            drawOne.BackColor = Color.SlateGray;
+            drawTwo.BackColor = Color.SlateGray;
+            drawThree.BackColor = Color.SlateGray;
+            drawFour.BackColor = Color.SlateGray;
+            drawFive.BackColor = Color.SlateGray;
+            drawSix.BackColor = Color.SlateGray;
+            drawSeven.BackColor = Color.SlateGray;
+            drawEight.BackColor = Color.SlateGray;
+            playButton.BackColor = Color.SlateGray;
+            autoResetButton.BackColor = Color.SlateGray;
+            resetButton.BackColor = Color.Red;
+        }
+
+        void resetColors()
+        {
+            pickOne.BackColor = Color.SlateGray;
+            pickTwo.BackColor = Color.SlateGray;
+            pickThree.BackColor = Color.SlateGray;
+            pickFour.BackColor = Color.SlateGray;
+            pickFive.BackColor = Color.SlateGray;
+            pickSix.BackColor = Color.SlateGray;
+            pickSeven.BackColor = Color.SlateGray;
+            pickEight.BackColor = Color.SlateGray;
+            drawOne.BackColor = Color.SlateGray;
+            drawTwo.BackColor = Color.SlateGray;
+            drawThree.BackColor = Color.SlateGray;
+            drawFour.BackColor = Color.SlateGray;
+            drawFive.BackColor = Color.SlateGray;
+            drawSix.BackColor = Color.SlateGray;
+            drawSeven.BackColor = Color.SlateGray;
+            drawEight.BackColor = Color.SlateGray;
+            playButton.BackColor = Color.SlateGray;
+            autoResetButton.BackColor = Color.SlateGray;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playButton.BackColor = Color.OrangeRed;
+            disableButtons();
+            draw = (byte)random.Next(0, 255);
+            setDrawColors();
+            if(draw == picks)
+            {
+                winScenario();
+            }
+            else
+            {
+                loseScenario();
+            }
+        }
+
+        private void winScenario()
+        {
+            string caption = "Winner!";
+            string message = "You won the lottery!";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+            result = MessageBox.Show(message, caption, buttons);
+            if (autoReset) reset();
+        }
+
+        private void loseScenario()
+        {
+            string caption = "You lose...";
+            string message = "You've lost :/. Try again!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == DialogResult.No) Application.Exit();
+            if (autoReset) reset();
+        }
+
+        private void disableButtons()
+        {
+            playButton.Enabled = false;
+            autoResetButton.Enabled = false;
+            pickOne.Enabled = false;
+            pickTwo.Enabled = false;
+            pickThree.Enabled = false;
+            pickFour.Enabled = false;
+            pickFive.Enabled = false;
+            pickSix.Enabled = false;
+            pickSeven.Enabled = false;
+            pickEight.Enabled = false;
+        }
+
+        private void enableButtons()
+        {
+            playButton.Enabled = true;
+            autoResetButton.Enabled = true;
+            pickOne.Enabled = true;
+            pickTwo.Enabled = true;
+            pickThree.Enabled = true;
+            pickFour.Enabled = true;
+            pickFive.Enabled = true;
+            pickSix.Enabled = true;
+            pickSeven.Enabled = true;
+            pickEight.Enabled = true;
+        }
+
+        private void pickOne_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(0);
+            if (picks.IsBitSet(0))
+            {
+                pickOne.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickOne.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickTwo_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(1);
+            if (picks.IsBitSet(1))
+            {
+                pickTwo.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickTwo.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickThree_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(2);
+            if (picks.IsBitSet(2))
+            {
+                pickThree.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickThree.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickFour_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(3);
+            if (picks.IsBitSet(3))
+            {
+                pickFour.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickFour.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickFive_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(4);
+            if (picks.IsBitSet(4))
+            {
+                pickFive.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickFive.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickSix_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(5);
+            if (picks.IsBitSet(5))
+            {
+                pickSix.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickSix.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickSeven_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(6);
+            if (picks.IsBitSet(6))
+            {
+                pickSeven.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickSeven.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void pickEight_Click(object sender, EventArgs e)
+        {
+            picks = picks.ToggleBit(7);
+            if (picks.IsBitSet(7))
+            {
+                pickEight.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                pickEight.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void autoResetButton_Click(object sender, EventArgs e)
+        {
+            if (autoReset)
+            {
+                autoReset = false;
+            }
+            else
+            {
+                autoReset = true;
+            }
+            if (autoReset)
+            {
+                autoResetButton.BackColor = Color.OrangeRed;
+            }
+            else
+            {
+                autoResetButton.BackColor = Color.SlateGray;
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void setDrawColors()
+        {
+            if (draw.IsBitSet(0)) drawOne.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(1)) drawTwo.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(2)) drawThree.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(3)) drawFour.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(4)) drawFive.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(5)) drawSix.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(6)) drawSeven.BackColor = Color.OrangeRed;
+            if (draw.IsBitSet(7)) drawEight.BackColor = Color.OrangeRed;
+        }
+
+        private void reset()
+        {
+            disableButtons();
+            autoReset = false;
+            draw = 0;
+            picks = 0;
+            resetColors();
+            enableButtons();
+        }
+    }
+
+    public static class byteOperations
+    {
+        public static byte ToggleBit(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+
+            return (byte)(b ^ (1 << pos));
+        }
+        public static bool IsBitSet(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+
+            return (b & (1 << pos)) != 0;
+        }
+    }
+}
